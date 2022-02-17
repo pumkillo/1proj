9ea72ca59fe59f0530b1f23479812618e061c4dd
gr492_smv <moshonkasedova@mail.ru>
2022-02-17 22:31:10 +0700

    refactor api functions and database extended

--- app/Controller/Api.php
+++ app/Controller/Api.php
@@ -3,6 +3,7 @@
 namespace Controller;
 
 use Model\Room;
+use Model\User;
 use Src\Request;
 use Src\View;
 use Src\Auth\Auth;
@@ -24,15 +25,13 @@ class Api
     public function login(Request $request): void
     {
         $header = getallheaders();
-        if (isset($header['bearer_token']) && !empty($header['bearer_token'])) {
+        if (isset($header['bearer_token']) && !empty($header['bearer_token']) && User::where('token', $header['bearer_token'])->count() === 1) {
             (new View())->toJSON(['status_code' => '400', 'data' => ['message' => 'You\'re already logged in.']]);
         }
-        if (!isset($_POST['login']) or !isset($_POST['password'])) {
-            (new View())->toJSON(['status_code' => '401', 'data' => ['error' => 'Incorrect data.']]);
-        }
-
         if (Auth::attempt($_POST)) {
+            $user = User::where('login', '=', $_POST['login']);
             $token = Auth::generateBearer();
+            $user->update(['token' => md5(time())]);
             (new View())->toJSON(['status_code' => '200', 'data' => ['token' => $token]]);
         }
         (new View())->toJSON(['status_code' => '401', 'data' => ['error' => 'Incorrect data.']]);
@@ -41,11 +40,23 @@ class Api
     public function seats_filter(Request $request): void
     {
         $header = getallheaders();
-        if (!isset($header['bearer_token']) || empty($header['bearer_token'])) {
+        if (!isset($header['bearer_token']) || empty($header['bearer_token']) || User::where('token', $header['bearer_token'])->count() === 0) {
             (new View())->toJSON(['status_code' => '403', 'data' => ['error' => 'You should be logged in to visit this page.']]);
             return;
         }
         $room_seats = Room::where('division_id', '=', $request->get('division_id'))->sum('amount_of_seats');
-        (new View())->toJSON(['status_code' => '200', 'data' => ['data' => $room_seats]]);
+        (new View())->toJSON(['status_code' => '200', 'data' => ['count if seats' => $room_seats]]);
+    }
+
+    public function logout(Request $request): void
+    {
+        $header = getallheaders();
+        if (!isset($header['bearer_token']) || empty($header['bearer_token']) || User::where('token', $header['bearer_token'])->count() === 0) {
+            (new View())->toJSON(['status_code' => '400', 'data' => ['error' => 'You should be logged in to log out.']]);
+            return;
+        }
+        $user = User::where('token', $header['bearer_token']);
+        $user->update(['token' => NULL]);
+        (new View())->toJSON(['status_code' => '200', 'data' => ['message' => 'You have been logged out']]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ Route::add('GET', '/', [Controller\Api::class, 'index']);
 Route::add('POST', '/echo', [Controller\Api::class, 'echo']);
 Route::add('POST', '/login', [Controller\Api::class, 'login']);
 Route::add('GET', '/seats-filter', [Controller\Api::class, 'seats_filter']);
+Route::add('GET', '/logout', [Controller\Api::class, 'logout']);
